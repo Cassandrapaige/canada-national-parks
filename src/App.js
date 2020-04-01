81fa8205e572cc47ad700e5b764bf7af5b5a4a22
Cassandra Rossall <cassandrarossall@Cassandras-MacBook-Pro.local>
2020-04-01 18:30:21 +1300

    National park api data, inital set-up and design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import {animated, useSpring, config} from 'react-spring'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {COUNTRIES} from './data/data'
+import '../src/App.css'
+
+const App = () => {
+  const [data, setData] = useState(COUNTRIES);
+  const [num, setNum] = useState(1)
+  const [isActive, setIsActive] = useState(false)
+  const [card, setCard] = useState('')
+  const [x, setX] = useState('50px')
+  const [y, setY] = useState('50px')
+  const [toggle, setToggle] = useState(false);
+  const [index, setIndex] = useState();
+  const [image, setImage] = useState(null)
+  const [title, setTitle] = useState('')
+  
+  const handleType = type => {
+    type === 'minus' ? setNum(prevNum => prevNum - 1)
+    : setNum(prevNum => prevNum + 1);
+    setToggle(false)
+  }
+
+  const showImgContent = (type, event) => {
+     setCard(type)
+     setIsActive(true)
+     setX(event.clientX + 'px')
+     setY(event.clientY + 'px')
+  };
+
+  const handleClick = ((index, event) => {
+    setToggle(!toggle)
+    setIndex(index);
+    setImage(event.target.src)
+    setTitle(event.target.alt)
+    setX('100%')
+    setY('100%')
+  })
+
+
+  const fadeIn = useSpring({
+    from: {
+      opacity: 0,
+      transform: 'translateY(50px)',
+      color: '#fff',
+    },
+    to: {
+      opacity: toggle ? 1 : 0,
+      transform: toggle ? 'translateY(0px)' : 'translateY(50px)',
+    },
+    config: config.wobbly,
+  })
+
+  const props = useSpring({
+    config: config.gentle,
+    from: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      background: 'rgba(19, 19, 19, .99)',
+      width: '0px',
+      height: '100%',
+      display: 'none',
+      borderRadius: '50%',
+      zIndex: '20',
+      opacity: 0
+    },
+    to: {
+      width: toggle ? '500px' : '500px',
+      borderRadius: '0%',
+      display: toggle ? 'block' : 'none',
+      opacity: 1
+    },
+    delay: 300
+  })
+
+  return ( 
+    <> 
+    <div className="container" style = {{
+      backgroundImage: `url(${data[num].image})`
+    }}>
+      <div className="content">
+        <h1>{data[num].province}</h1>
+        <div className="about">
+          <h4>Capital: <span>{data[num].capital}</span> | </h4>
+          <h4>Population: <span>{data[num].population}</span></h4>
+        </div>
+
+        <div className="images">
+          {
+            data[num].images.map((image, index) => (
+              <img 
+                src={image.url} 
+                alt={image.caption} 
+                onClick = {(e) => {handleClick(index, e)}}
+                onMouseMove = {(event) => {showImgContent(image.caption , event)}}
+              />
+            ))
+          }
+        </div>
+      </div>
+
+      <div className="card" 
+            style = {{ position: 'absolute', top: y, left: x, opacity: isActive ? 1 : 0 }}>
+          <h4>{card}</h4>
+      </div>
+
+      <div className="buttons">
+        <button 
+          className = 'next' onClick = {() => {handleType('plus')}}>
+          {data[num + 1].province}
+        </button>
+        <button 
+          className = 'prev' onClick = {() => {handleType('minus')}}>
+          {data[num - 1].province}
+        </button>
+      </div>
     </div>
-  );
+    <animated.div style= {props} className="expand-content">
+      <animated.h3 style = {fadeIn}>{title}</animated.h3>
+      <animated.p style = {fadeIn}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, est, recusandae necessitatibus, itaque quidem pariatur laboriosam quasi amet neque laudantium saepe incidunt? Deleniti dignissimos quis inventore deserunt obcaecati iure ullam, voluptas asperiores dolorem aliquid! Similique corrupti consequatur maxime itaque dicta voluptate vero praesentium laudantium dolor? Voluptatum esse porro magnam omnis?</animated.p>
+        <animated.img src={image} alt={title} style = {fadeIn} />
+    </animated.div>
+</>
+  )
 }
 
 export default App;
---
+++ src/data/data.jsx
@@ -0,0 +1,315 @@
+export const COUNTRIES = [
+        {
+            id: 1,
+            province: 'British Columbia',
+            capital: 'Victoria',
+            image: 'https://media-exp1.licdn.com/dms/image/C561BAQHP3pw0Tn6GeA/company-background_10000/0?e=2159024400&v=beta&t=-v2mhcMh-dL802MJffKP5MB_pzI7225xrWM4E0R_D1s',
+            population: '5.071 million',
+            images: [
+                {
+                    url: 'https://i.pinimg.com/originals/3f/31/e0/3f31e042529aa46ba1fcb7dcfde4762d.jpg' ,
+                    caption: 'Yoho National Park',
+                },
+                {
+                    url: 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=1600&h=1067&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2020%2F01%2Fglacier-national-park-GLACIERSIGNS0120.jpg',
+                    caption: 'Glacier National Park'
+                },
+                {
+                    url: 'https://farm3.static.flickr.com/2808/12370941704_4765d3d851_c.jpg',
+                    caption: 'Gulf Islands National Park Reserve'
+                },
+                {
+                    url: 'https://ttnotes.com/images/gwaii-haanas-national-park-reserve-national-marine-conservation-area-reserve-haida-heritage-site-haida-gwaii-1.jpg' ,
+                    caption: 'Gwaii Haanas National Park Reserve',
+                },
+                {
+                    url: 'https://www.kamloopshikingclub.net/wp-content/uploads/2018/10/Kootenay.National.Park_.original.1410-1024x683.jpg',
+                    caption: 'Kootenay National Park'
+                },
+                {
+                    url: 'https://www.kootenayrockies.com/wp-content/uploads/2020/01/184_REVZNTLZPARK.jpg',
+                    caption: 'Mount Revelstoke National Park'
+                },
+                {
+                    url: 'https://mustseeplaces.eu/wp-content/uploads/2016/02/pacific-rim-national-park-canada.jpg',
+                    caption: 'Pacific Rim National Park Reserve'
+                }
+            ]
+        },
+        {
+            id: 2,
+            province: 'Alberta',
+            capital: 'Edmonton',
+            image: 'https://c4.wallpaperflare.com/wallpaper/322/27/794/canada-alberta-mountains-nature-water-hd-wallpaper-preview.jpg',
+            population: '4.371 million',
+                images: [
+                {
+                    url: 'https://lp-cms-production.imgix.net/2019-06/79941051.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+                    caption: 'Banff National Park'
+                },
+                {
+                    url: 'https://assets.exploreedmonton.com/images/_1200x630_crop_center-center_none/Elk-Island.jpg?utime=20180927231508' ,
+                    caption: 'Elk Island National Park',
+                },
+                {
+                    url: 'https://gypsyguide.com/wp-content/uploads/2017/09/Jasper-National-Park-Tour.jpg',
+                    caption: 'Jasper National Park'
+                },
+                {
+                    url: 'https://roadscholar-iv-prod.azureedge.net/publishedmedia/30u3e4rpccd2vqd8kc4u/19946-glacier-waterton-lakes-national-parks-lghoz.jpg' ,
+                    caption: 'Waterton Lakes National Park',
+                },
+                {
+                    url: 'https://outpostmagazine.com/wp-content/uploads/2017/06/sup-wood-buffalo-1200x520.jpg',
+                    caption: 'Wood Buffalo National Park'
+                }
+            ]
+        },
+        {
+            id: 3,
+            province: 'Saskatchewan',
+            capital: 'Regina',
+            image: 'https://canada.constructconnect.com/app/uploads/2018/08/SASKATCHEWAN-e1534514927750.jpg',
+            population: '1.174 million',
+                images: [
+                {
+                    url: 'https://www.todocanada.ca/wp-content/uploads/Grasslands-National-Park--1280x720.jpg',
+                    caption: 'Grasslands National Park'
+                },
+                {
+                    url: 'https://www.hinzie.com/media/image/199170_max.JPG' ,
+                    caption: 'Prince Albert National Park',
+                }
+            ]
+        },
+        {
+            id: 4,
+            province: 'Manitoba',
+            capital: 'Winnipeg',
+            image: 'https://tce-live2.s3.amazonaws.com/media/media/a5b2bc47-c5be-4499-b2a0-f516d71e6330.jpg',
+            population: '1.369 million',
+                images: [
+                {
+                    url: 'https://www.pc.gc.ca/en/pn-np/mb/riding/nature/video/%22//pcacdn.azureedge.net/-/media/VideoItems/Images/pn-np/mb/riding/bald-hill-2017-plc.jpg?modified=20171005150353%22',
+                    caption: 'Riding Mountain Nationala Park'
+                },
+                {
+                    url: 'https://www.nationalgeographic.com/content/dam/travel/photos/000/367/36747.ngsversion.1481144417807.adapt.1900.1.jpg' ,
+                    caption: 'Wapusk National Park',
+                }
+            ]
+        },
+        {
+            id: 5,
+            province: 'Ontario',
+            capital: 'Ottawa',
+            image: 'https://ontarionature.org/wp-content/uploads/2018/07/Killarney_PP_2_Claudiu_Dobre_CC_BY-NC-SA_2.0.jpg',
+            population: '14.57 million',
+                images: [
+                {
+                    url: 'https://i.pinimg.com/originals/d6/31/52/d63152cc54493ebbe699513671136999.jpg',
+                    caption: 'Bruce Penninsula National Park'
+                },
+                {
+                    url: 'https://www.todocanada.ca/wp-content/uploads/Georgian-Bay-Islands-1280x720.jpg' ,
+                    caption: 'Georgian Bay Islands National Park',
+                },
+                {
+                    url: 'https://cangeo-media-library.s3.amazonaws.com/s3fs-public/point_pelee_cangeo-11.jpg',
+                    caption: 'Point Pelee National Park'
+                },
+                {
+                    url: 'https://www.cbc.ca/kidscbc2/content/the_feed/pukaskwa_location-min.jpg',
+                    caption: 'Pukaskwa National Park'
+                },
+                {
+                    url: 'https://www.thesetinystories.com/wp-content/uploads/2017/10/rouge-web-2-of-2.jpg' ,
+                    caption: 'Rouge National Urban Park',
+                },
+                {
+                    url: 'https://islandlifemag.ca/wp-content/uploads/2018/11/thwartway-IL-18.jpg',
+                    caption: 'Thousand Islands National Park'
+                }
+            ]
+        },
+        {
+            id: 6,
+            province: 'Quebec',
+            capital: 'Quebec City',
+            image: 'https://www.recyclemyelectronics.ca/qc/wp-content/uploads/sites/18/2018/12/PQ-Longueil.jpg',
+            population: '8.485 million',
+                images: [
+                {
+                    url: 'https://travelguide.michelin.com/sites/default/files/styles/poi_slideshow_big/public/images/travel_guide/NX-14417.jpg?itok=Vr_sCfF3',
+                    caption: 'Forillon National Park'
+                },
+                {
+                    url: 'https://d38k3gcqg7i488.cloudfront.net/wp-content/uploads/2018/05/parc-national-mauricie-tm.jpg' ,
+                    caption: 'La Mauricie National Park',
+                },
+                {
+                    url: 'https://i.pinimg.com/474x/8d/8f/95/8d8f95fc99817f571420483dbaf11373.jpg',
+                    caption: 'Mingan Archipelago National Park Reserve'
+                }
+            ]
+        },
+        {
+            id: 7,
+            province: 'Newfoundland and Labrador',
+            capital: "St.John's",
+            image: 'https://s3.amazonaws.com/exposure-media/production/posts/250533/cover-photo/cover-1515677490.jpg?w=900&q=50&sharp=10&fit=crop&fm=pjpg&auto=format',
+            population: '521,542',
+                images: [
+                {
+                    url: 'https://www.chimuadventures.com/sites/default/files/styles/hotel_images/437_Lab_Coast.jpg',
+                    caption: 'Akami-Uapishkᵁ-KakKasuak-Mealy Mountains National Park Reserve'
+                },
+                {
+                    url: 'https://d36tnp772eyphs.cloudfront.net/blogs/1/2019/05/Western-Brook-Pond-Fjord-Hikers-Western-3.jpg' ,
+                    caption: 'Gros Morne National Park',
+                },
+                {
+                    url: 'https://img.ev.mu/images/attractions/6440/960x640/433613.jpg',
+                    caption: 'Terra Nova National Park'
+                },
+                {
+                    url: 'https://www.chimuadventures.com/sites/default/files/styles/hotel_images/437_Torngat_2.jpg',
+                    caption: 'Torngat Mountains National Park'
+                }
+            ]
+        },
+        {
+            id: 8,
+            province: 'New Brunswick',
+            capital: 'Fredericton',
+            image: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/hopewell-rocks-darlene-munro.jpg',
+            population: '776,827',
+                images: [
+                {
+                    url: 'https://cottagelife.com/wp-content/uploads/2013/11/Hopewell-Rocks.jpg',
+                    caption: 'Fundy National Park'
+                },
+                {
+                    url: 'https://i1.wp.com/rvplacestogo.com/wp-content/uploads/2018/03/boardwalk.jpg?resize=1038%2C576' ,
+                    caption: 'Kouchibouguac National Park',
+                }
+            ]
+        },
+        {
+            id: 9,
+            province: 'Nova Scotia',
+            capital: 'Halifax',
+            image: 'https://img.theculturetrip.com/768x432/wp-content/uploads/2017/06/lighthouse-1872998_1280-e1499074063907.jpg',
+            population: '971,395',
+                images: [
+                {
+                    url: 'https://capebreton.lokol.me/Content/Uploads/XV5PDES5VUCHAMQQRS2QRNDW6GAEQ6GX/Media/48/feature%20cape%20breton%20highlands%20best%20national%20park%20usa%20today.jpg',
+                    caption: 'Cape Breton Highlands National Park'
+                },
+                {
+                    url: 'https://c1.staticflickr.com/5/4119/4802543427_9ceea5946f_b.jpg' ,
+                    caption: 'Kejimkujik National Park',
+                },
+                {
+                    url: 'https://images.thestar.com/uc0irArqP9syOls456ov9QN91D4=/1200x675/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/news/canada/2012/05/04/sable_island_a_national_park_that_we_could_love_to_death/sableisland4.jpeg',
+                    caption: 'Sable Island National Park Reserve'
+                }
+            ]
+        },
+        {
+            id: 10,
+            province: 'Prince Edward Island',
+            capital: 'Charlottetown',
+            image: 'https://4597wq4asyz01jes8rz3yr94-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/Optimized-PEI.jpg',
+            population: '156,947',
+                images: [
+                {
+                    url: 'https://live.staticflickr.com/884/27658132758_b1170279fe_b.jpg',
+                    caption: 'Prince Edward Island National Park'
+                }
+            ]
+        },
+        {
+            id: 11,
+            province: 'Yukon',
+            capital: 'Whitehorse',
+            image: 'https://cdn2.wanderlust.co.uk/media/1045/dreamstime_xl_25285244.jpg?anchor=center&mode=crop&width=1200&height=0&rnd=131792461070000000',
+            population: '35,874',
+                images: [
+                {
+                    url: 'https://img.wallbg.com/wallpaper/previews/2017/Muskeg-Creek-Tors-Ivvavik-National-Park-Yukon-Canada-1866691175.jpg',
+                    caption: 'Ivvavik National Park'
+                },
+                {
+                    url: 'https://www.heritageyukon.ca/sites/default/files/Kluane%20NPR%20c.%20Parks%20Canada%20F.Mueller.jpg' ,
+                    caption: 'Kluane National Park and Reserve',
+                },
+                {
+                    url: 'https://pbs.twimg.com/media/B1eMHW1IcAAzg2H.jpg',
+                    caption: 'Vuntut National Park'
+                }
+            ]
+        },
+        {
+            id: 12,
+            province: 'Northwest Territories',
+            capital: 'Yellowknife',
+            image: 'https://www.travelinglifestyle.net/wp-content/uploads/2019/04/Nahanni-National-Park-Northwest-Territories.jpg',
+            population: '44,826',
+                images: [
+                {
+                    url: 'https://live.staticflickr.com/8288/7880481226_534fd22df5_b.jpg',
+                    caption: 'Aulavik National Park'
+                },
+                {
+                    url: 'https://www.pc.gc.ca/en/pn-np/nt/naatsihchoh/info/~/media/584E956DDB15421CA5E99253F91FF7DB.ashx' ,
+                    caption: "Nááts'ihch'oh National Park Reserve",
+                },
+                {
+                    url: 'https://www.nationalgeographic.com/content/dam/travel/photos/000/369/36945.ngsversion.1481146233342.adapt.1900.1.jpg',
+                    caption: 'Nahanni National Park Reserve'
+                },
+                {
+                    url: 'https://spectacularnwt.com/sites/default/files/eastarm_1.jpg',
+                    caption: 'Thaidene Dene National Park Reserve'
+                },
+                {
+                    url: 'https://spectacularnwt.com/sites/default/files/styles/hero_desktop/public/tn-050814-15.jpg?itok=7PWczHJI' ,
+                    caption: 'Tuktut Nogait National Park',
+                },
+                {
+                    url: 'https://media.globalcitizen.org/thumbnails/62/f4/62f44692-e7fc-4d5d-a0e7-463adb682722/wood_buffalo_national_park_getty_social.png__1500x670_q85_crop_subsampling-2.png',
+                    caption: 'Wood Buffalo National Park'
+                }
+            ]
+        },
+        {
+            id: 13,
+            province: 'Nunavut',
+            capital: 'Iqaluit',
+            image: 'https://uphere.ca/sites/default/files/p20-grand-prize-1-artur-stanisz_0.jpg',
+            population: '38,780',
+                images: [
+                {
+                    url: 'https://s3.amazonaws.com/freshdaily-images/prism-thumbnails/static/articles/20200116-mountthor_3.jpg-resize_then_crop-_frame_bg_color_FFF-h_1365-gravity_center-q_70-preserve_ratio_true-w_2048_.webp',
+                    caption: 'Auyuittuq National Park'
+                },
+                {
+                    url: 'https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_e0xsaryh/def_height/1600/def_width/2400/version/100011/type/1' ,
+                    caption: 'Qausuittuq National Park',
+                },
+                {
+                    url: 'https://www.todocanada.ca/wp-content/uploads/Quttinirpaaq-National-Park.jpg',
+                    caption: 'Quttinirpaaq National Park'
+                },
+                {
+                    url: 'https://www.todocanada.ca/wp-content/uploads/Sirmilik-National-Park.jpg' ,
+                    caption: 'Sirmilik National Park',
+                },
+                {
+                    url: 'https://nunatsiaq.com/wp-content/uploads/2019/01/1-Main-Picture_Picture-by-Jovan-Simic-2-1024x605.jpg',
+                    caption: 'Ukkusiksalik National Park'
+                }
+            ]
+        }
+    ]
